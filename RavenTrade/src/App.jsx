9c80be57ced42f913d81b0c639f3b49c2f128089
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2024-02-21 19:05:32 -0800

    faraz main branch changes

--- RavenTrade/src/App.jsx
+++ RavenTrade/src/App.jsx
@@ -1,6 +1,6 @@
-import {Route, Routes} from "react-router-dom";
-import Signup from "./pages/signup.jsx";
-import Login from "./pages/login.jsx";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
 import Products from "./pages/Products.jsx";
 import MarketPlace from "./pages/MarketPlace.jsx";
@@ -9,25 +9,49 @@ import MainLayout from "./pages/MainLayout.jsx";
 import ProtectedRoute from "./componenents/ProtectedRoute.jsx";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/signup" element={<Signup/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/home" element={<ProtectedRoute component={<MainLayout childComponent={<Home/>}/>}/>}/>
+  return (
+    <>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute
+              component={<MainLayout childComponent={<Home />} />}
+            />
+          }
+        />
 
-                <Route path="/marketplace"
-                       element={<ProtectedRoute component={<MainLayout childComponent={<MarketPlace/>}/>}/>}/>
+        <Route
+          path="/marketplace"
+          element={
+            <ProtectedRoute
+              component={<MainLayout childComponent={<MarketPlace />} />}
+            />
+          }
+        />
 
-                <Route path="/myproducts"
-                       element={<ProtectedRoute component={<MainLayout childComponent={<Products/>}/>}/>}/>
+        <Route
+          path="/myproducts"
+          element={
+            <ProtectedRoute
+              component={<MainLayout childComponent={<Products />} />}
+            />
+          }
+        />
 
-                <Route path="/additem"
-                       element={<ProtectedRoute component={<MainLayout childComponent={<AddItem/>}/>}/>}/>
-
-            </Routes>
-        </>
-    );
+        <Route
+          path="/additem"
+          element={
+            <ProtectedRoute
+              component={<MainLayout childComponent={<AddItem />} />}
+            />
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
